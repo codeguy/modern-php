d0af3e5b6bf9f8af42e7b9c04c9bde3048b87b00
Josh Lockhart <info@joshlockhart.com>
2014-12-14 10:59:07 -0500

    Add deployment app example

---
+++ 09-deployment/config/deploy.rb
@@ -0,0 +1,46 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'APP'
+set :repo_url, 'git@github.com:USERNAME/REPO.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deploy/apps/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+  desc "Build"
+  after :updated, :build do
+      on roles(:web) do
+          within release_path  do
+            execute :composer, "install --no-dev --quiet"
+          end
+      end
+  end
+end
---
+++ 09-deployment/public/index.php
@@ -0,0 +1,2 @@
+<?php
+echo "Capistrano works!";
